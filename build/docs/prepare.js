b3b486d14ac469e0b175e450b4a720e7b8c165bb
Volodymyr Minchenko <voldemarprk2001@gmail.com>
2021-11-13 01:08:41 +0200

    refactor: code style of prev functionality

--- build/docs/prepare.js
+++ build/docs/prepare.js
@@ -1,6 +1,10 @@
-const schemas = require("../../lib/schemas")
-const fs = require("fs")
-Object.keys(schemas).forEach( key => {
-	console.log(`Prepare schema ./docs/schemas/json/${key}.schema.json`)
-	fs.writeFileSync(`./docs/schemas/json/${key}.schema.json`, JSON.stringify(schemas[key].schema, null, " "))	
-})
+const fs = require('fs');
+const schemas = require('../../lib/schemas');
+
+Object.keys(schemas).forEach((key) => {
+  console.log(`Prepare schema ./docs/schemas/json/${key}.schema.json`);
+  fs.writeFileSync(
+    `./docs/schemas/json/${key}.schema.json`,
+    JSON.stringify(schemas[key].schema, null, ' '),
+  );
+});
--- e1.js
+++ e1.js
@@ -1,121 +1,109 @@
-const { Consumer, Publisher, Middleware, yaml2js } = require("./lib")
+/* eslint-disable no-unused-vars */
+const { Consumer, Publisher, Middleware, yaml2js } = require('./lib');
 
 const amqp = {
-		url: "amqps://xoilebqg:Nx46t4t9cxQ2M0rF2rIyZPS_xbAhmJIG@hornet.rmq.cloudamqp.com/xoilebqg"
-	}
+  url: 'amqps://xoilebqg:Nx46t4t9cxQ2M0rF2rIyZPS_xbAhmJIG@hornet.rmq.cloudamqp.com/xoilebqg',
+};
 
-	const exchange = {
-		name: "amqp_test_exchange"
-	}
+const exchange = {
+  name: 'amqp_test_exchange',
+};
 
+const consumerOptions = {
+  amqp,
+  queue: {
+    exchange,
+  },
+};
 
-	const consumerOptions = {
-		amqp,
-		queue:{
-			exchange
-		}
-	}
-
-	const publisherOptions = {
-		amqp,
-		exchange
-	}
-
+const publisherOptions = {
+  amqp,
+  exchange,
+};
 
 const schema = {
-	type: "object",
-	required: ["data"],
-	properties:{
-		data: {
-			type: "string"
-		}
-	},
-	errorMessage:{
-		type: "The sended message should be a object",
-		properties:{
-			data: "The sended message data should be a string"
-		}
-	}
-}
-
+  type: 'object',
+  required: ['data'],
+  properties: {
+    data: {
+      type: 'string',
+    },
+  },
+  errorMessage: {
+    type: 'The sended message should be a object',
+    properties: {
+      data: 'The sended message data should be a string',
+    },
+  },
+};
 
 const inputSchema = yaml2js(`
+  type: object
+  required:
+    - data
+  properties:
+    data:
+      type: string
+  errorMessage:
+    type: The consumed message should be a object
+    properties:
+      data: The consumed message data should be a string
+`);
 
-		type: object
-		required:
-			- data
-		properties:
-			data:
-				type: string
-		errorMessage:
-			type: The consumed message should be a object			
-			properties:
-				data: The consumed message data should be a string
+const run = async () => {
+  const consumer = await new Consumer(consumerOptions);
 
-`)
+  await consumer
+    .use(Middleware.Json.parse)
 
+    .use((err, msg, next) => {
+      console.log('Consume: ', msg.content);
+      msg.ack();
+      next();
+    })
 
-let run  = async () => {
+    .use([
+      Middleware.Schema.validator(inputSchema),
+      Middleware.Error.Log,
+      Middleware.Error.BreakChain,
+      Middleware.Filter((msg) => msg.content && msg.content.data.endsWith('5')),
+    ])
 
-	
-	let consumer = await new Consumer(consumerOptions)
-	
-	await consumer
-			.use( Middleware.Json.parse )
-			
-			
-			.use( ( err, msg, next) => {
-				console.log("Consume: ", msg.content)
-				msg.ack()
-				next()
-			})
+    .use(async (err, msg, next) => {
+      console.log('Process:', msg.content);
+    })
 
-			.use( 
-				[ 
-					Middleware.Schema.validator(inputSchema),
-					Middleware.Error.Log,
-					Middleware.Error.BreakChain,
-					Middleware.Filter( msg => {
-						return msg.content && msg.content.data.endsWith("5")
-					}),
-				]	
-			)
-			
-			.use( async ( err, msg, next) => {
-				console.log("Process:", msg.content)
-			})
-			
-			.start()
-	
+    .start();
 
-	let publisher = await new Publisher(publisherOptions)
-	publisher
-		
-		// .use( [ Middleware.Schema.validator(schema), Middleware.Error.Log, Middleware.Error.BreakChain ] )
+  const publisher = await new Publisher(publisherOptions);
+  publisher
 
-		.use (( err, msg, next) => {
-			console.log("Send:", msg.content)
-			next()
-		})
-						
-		.use(Middleware.Json.stringify)
+    // .use([
+    //   Middleware.Schema.validator(schema),
+    //   Middleware.Error.Log,
+    //   Middleware.Error.BreakChain,
+    // ])
 
+    .use((err, msg, next) => {
+      console.log('Send:', msg.content);
+      next();
+    })
 
-	for(let i=1; i<=5; i++){
-		await publisher.send({
-			data:`test message ${i}`
-		})	
-	}
-	
-	await publisher.send("hello")	
-	await publisher.send({data:10})	
-	
+    .use(Middleware.Json.stringify);
 
-	setTimeout(async () => {
-		await publisher.close()
-		await consumer.close()
-	}, 1000)
-	
-}
+  for (let i = 1; i <= 5; i++) {
+    await publisher.send({
+      data: `test message ${i}`,
+    });
+  }
 
-run()
+  await publisher.send('hello');
+  await publisher.send({ data: 10 });
+
+  setTimeout(async () => {
+    await publisher.close();
+    await consumer.close();
+  }, 1000);
+};
+
+run();
